4d686cf4938f4f1ddb2752b72086dd1e40ac4c21
adityakeshri121 <adityakeshrisam1729@gmail.com>
2024-02-11 17:41:45 +0530

    added search and list in ui

--- index.js
+++ index.js
@@ -7,19 +7,41 @@ async function handleSearchChange(input) {
     // Perform additional actions with the search value as needed
     if (searchValue.length === 1) {
         try {
-
             const fetchData = await fetch(`https://themealdb.com/api/json/v1/1/search.php?f=${searchValue}`);
             const jsonData = await fetchData.json();
-            const addData = jsonData.meals.map(one => `<li>${one.strMeal}</li>`)
-            console.log(addResult.innerHTML)
-            addResult.innerHTML = addData.map(elem => elem)
+            const addData = jsonData.meals.map(one => ` <div class="each-dish">
+            <div id="search-meal" onclick="searchMeal(this)">${one.strMeal}</div>
+            <div id="fav-icon" onclick="addToFav(this)"><i class="fa-regular fa-heart"></i></div>
+        </div>`)
+            addResult.innerHTML = addData.map(elem => elem);
+
+
         }
         catch (e) {
             console.log(e);
+            addResult.innerHTML = 'No result found!'
         }
     }
 
     if (searchValue.length === 0) {
-        addResult.innerHTML = 'No result found'
+        addResult.innerHTML = 'Search Anything'
+    }
+}
+
+function addToFav(e) {
+    console.log('added to fav')
+    console.log(e)
+}
+
+function searchMeal(e) {
+    const mealName = e.innerHTML;
+    console.log(mealName)
+    try {
+        const searchByName = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        const searchByNameJson = searchByName.json();
+
+    }
+    catch (e) {
+
     }
 }
