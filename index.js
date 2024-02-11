f288af84d4db5cd7c1a69c7dd3cf2d1a92922a64
adityakeshri121 <adityakeshrisam1729@gmail.com>
2024-02-11 16:42:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function handleSearchChange(input) {
     // Perform additional actions with the search value as needed
     if (searchValue.length === 1) {
         try {
+
             const fetchData = await fetch(`https://themealdb.com/api/json/v1/1/search.php?f=${searchValue}`);
             const jsonData = await fetchData.json();
             const addData = jsonData.meals.map(one => `<li>${one.strMeal}</li>`)
